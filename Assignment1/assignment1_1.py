cfeeede0d907b9545970f2d7ea8415f2985f5067
gtokusum <gtokusum@iu.edu>
2020-09-15 19:12:17 -0400

    Assignment 1 finished

--- Assignment1/assignment1_1.py
+++ Assignment1/assignment1_1.py
@@ -2,40 +2,40 @@
 #RETURN A and B
 def myAnd(A,B):
 #TO DO: Implement function
-
+    return A and B
 
 #INPUT two values
 #RETURN A or B 
 def myOr(A,B):
 #TO DO: Implement function
-
+    return A or B
 
 #INPUT one value
 #RETURN not A
 def myNot(A):
 #TO DO: Implement function
-
+return not A
 
 #INPUT two values
 #RETURN not (A and B) (called nand)
 #REQUREMENTS use only previous functions
 def myNand(A,B):
 #TO DO: Implement function
-
+    return not (A and B)
 
 #INPUT two values
 #RETURN not (A or B) (called nor)
 #REQUIREMENTS use only previous functions
 def myNor(A,B):
 #TO DO: Implement function
-
+    return not (A or B)
 
 #INPUT two values A,B
 #Return material implication A => B 
 #REQUIREMENTS use only previous functions
 def myImplication(A,B):
 #TO DO: Implement function
-
+    return A >= B
 
 #EXTRA CREDIT 5%
 #INPUT two values A,B
@@ -43,3 +43,4 @@ def myImplication(A,B):
 #REQUIREMENTS only use myNand gate
 def ECmyWeirdOr(A,B):
 #TO DO: Implement function
+    return not myNand(A,B)
