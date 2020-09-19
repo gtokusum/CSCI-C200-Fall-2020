a1f3c586294dd7a233ff678ef1a99e6c59110df2
mjenne <jennemj@gmail.com>
2020-09-19 12:35:16 -0400

    Create file: a2_assignment.py

---
+++ Assignment2/a2_assignment.py
@@ -0,0 +1,41 @@
+#PROBLEM 1 a2_1
+#INPUT speed s as integer
+#RETURN change in speed as integer
+def speed(s):
+#TO DO Implement function
+    pass
+
+
+
+#PROBLEM 2 a2_2
+#INPUT temp in Fahrenheit float
+#RETURN temp in Celcius float
+def F_C(t):
+#TO DO Implement function
+    pass
+
+#a2_3
+#You cannot use Python's max function
+#You must use if-statement
+#INPUT two numbers
+#RETURN maximum of the two
+def max(x,y):
+#TO DO Implement function
+    pass
+
+#You cannot use Python's sum function
+#You must use a bounded loop
+#INPUT a non-empty list of numbers
+#RETURN the average
+def ave(xlst):
+#TO DO Implement function
+    pass
+
+#You must use your max function
+#You must use a bounded loop
+#INPUT non-empty list of numbers
+#RETURN maximum number in list
+def max_list(xlst):
+#TO DO Implement function
+    pass
+
