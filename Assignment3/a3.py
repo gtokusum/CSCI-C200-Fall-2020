bc9d0de947e132597f1c39c0e4158ec8a8c7017b
mjenne <jennemj@gmail.com>
2020-09-25 17:20:55 -0400

    Create file: a3.py

---
+++ Assignment3/a3.py
@@ -0,0 +1,71 @@
+"""
+TODO: Add comment about who your partner was (or if you worked alone). 
+Include anything else you want to in this comment
+
+"""
+
+import math
+
+
+###########################################################################
+# Functions for Problem 1
+###########################################################################
+def y(d,r,t):
+    pass
+
+def N(n_0, m, t):
+    pass
+
+def N_t(t):
+    pass
+
+def K(t):
+    pass
+
+def r(t):
+    pass
+
+def p(x):
+    pass
+
+def W(P_i, P_f):
+    pass
+
+def dep(c,s,el):
+    pass
+
+def L(V,A,C_l):
+    pass
+
+###########################################################################
+# Functions for Problem 2
+###########################################################################
+def q(a,b,c):
+    # This function will return a tuple of 2 values
+    # The "plus" value must be first in the tuple
+    # The "minus" value must be second in the tuple 
+    # Please note, this dealing with the +/- (plus/minus) sign in the equation
+    pass
+
+
+###########################################################################
+# Functions for Problem 3
+###########################################################################
+def checkout(xlst):
+    pass
+
+
+###########################################################################
+# Functions for Problem 4
+###########################################################################
+def e(xlst):
+    pass
+
+if __name__ == "__main__":
+    """
+    If you want to do some of your own testing in this file, 
+    please put any print statements you want to try in 
+    this if statement.
+
+    You **do not** have to put anything here
+    """
