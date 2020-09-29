eecc72cbe4e07f60ab28408ef58a3eb3598e5a81
gtokusum <gtokusum@iu.edu>
2020-09-29 12:45:54 -0400

    Finished without partner

--- Assignment3/a3.py
+++ Assignment3/a3.py
@@ -11,31 +11,36 @@ import math
 # Functions for Problem 1
 ###########################################################################
 def y(d,r,t):
-    pass
+    return d * math.exp(r*t)
 
 def N(n_0, m, t):
-    pass
+    return n_0*math.exp(m*t)
 
 def N_t(t):
-    pass
+    return math.ceil(71.8*math.exp(-8.98*math.exp(-0.0685*t)))
+
 
 def K(t):
-    pass
+    eq = (((9/2.6)*t)/((2*(t**2))+3))/100
+    return round(eq, 5)
 
 def r(t):
-    pass
-
+    eq = (1.5207*t**4) - (19.166*t**3) + (62.91*t**2) + (6.0726*t) + (1026)
+    return round(eq, 2)
+    
 def p(x):
-    pass
+    return (4*(x**2)) - (100*x) - (1000)
 
 def W(P_i, P_f):
-    pass
+    r = 8.314
+    return math.ceil(r * 300 * (math.log(P_i/P_f)))
 
 def dep(c,s,el):
-    pass
+    return round((c-s)/el,0)
 
 def L(V,A,C_l):
-    pass
+    k = 0.0033
+    return math.ceil(k * (V**2) * A * C_l)
 
 ###########################################################################
 # Functions for Problem 2
@@ -45,21 +50,46 @@ def q(a,b,c):
     # The "plus" value must be first in the tuple
     # The "minus" value must be second in the tuple 
     # Please note, this dealing with the +/- (plus/minus) sign in the equation
-    pass
-
+    d = (b**2) - (4*a*c)
+    if d >= 0:
+        x = (-b + math.sqrt(d)) / (2*a)
+        y = (-b - math.sqrt(d)) / (2*a)
+        return (x,y)
+    
+    else:
+        x = (-b/(2*a))  
+        xj = (math.sqrt(-d)/(2*a))
+        y = (-b/(2*a)) 
+        yj = - (math.sqrt(-1*d)/(2*a))
+        return (complex(x,xj),complex(y,yj))
 
 ###########################################################################
 # Functions for Problem 3
 ###########################################################################
 def checkout(xlst):
-    pass
+    price = 0
+    for alst in xlst:
+        if alst[2] == 1:
+            gamma = alst[0] * alst[1]
+            price += (gamma*1.07)
+        else:
+            gamma = alst[0] * alst[1]
+            price += gamma
+    return round(price,2) 
+
+
 
 
 ###########################################################################
 # Functions for Problem 4
 ###########################################################################
 def e(xlst):
-    pass
+    emptySeats = []
+    for alst in xlst:
+        for i in alst:
+            if i == 0:
+                emptySeats.append(i)
+    return len(emptySeats)
 
 if __name__ == "__main__":
     """
