448735c9892c76b2ddb6e7ea2bcb47e5a521331f
mjenne <jennemj@gmail.com>
2020-10-03 12:33:59 -0400

    Create file: a4_midterm.py

---
+++ Midterm/a4_midterm.py
@@ -0,0 +1,31 @@
+def ASCII_square(n):
+    pass
+
+
+def ASCII_triangle(n):
+    pass
+
+
+
+def my_range(start,stop,step):
+    pass
+
+
+Act, Au,Ag,Pd,Pt ="Act", "gold", "silver", "paladium", "platinum"
+spot_price = {Au:1918.40, Ag:24.23,\
+              Pt:912.45, Pd: 2353.35}
+portfolio = {"Act": 100000, Au:0, Ag:0, Pt:0, Pd:0}
+            
+def purchase(portfolio, metal, amt):
+    pass
+
+def worth(portfolio):
+    pass
+
+def how_much(portfolio, metal):
+    pass
+
+
+def find_num_min(xlst):
+    pass
+
