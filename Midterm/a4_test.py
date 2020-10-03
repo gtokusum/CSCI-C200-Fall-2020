59760a5bd0023e5760e3fe0a2c2dd1b3598146ce
mjenne <jennemj@gmail.com>
2020-10-03 16:23:31 -0400

    Update file: a4_test.py

--- Midterm/a4_test.py
+++ Midterm/a4_test.py
@@ -1,4 +1,4 @@
-import a4_exam
+import a4_midterm
 import unittest
 
 print("__________________")
@@ -20,38 +20,38 @@ p = {'Act': 63746.04999999999, 'gold': 4, 'silver': 20, 'platinum': 5, 'paladium
 class MyTester(unittest.TestCase):
 
     def test_my_range(self):
-        self.assertEqual(a4_exam.my_range(1,7,3),[1,4]) 
-        self.assertEqual(a4_exam.my_range(7,0,3),[]) 
-        self.assertEqual(a4_exam.my_range(0,10,1),[0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(a4_midterm.my_range(1,7,3),[1,4]) 
+        self.assertEqual(a4_midterm.my_range(7,0,3),[]) 
+        self.assertEqual(a4_midterm.my_range(0,10,1),[0,1,2,3,4,5,6,7,8,9])
 
     def test_ASCII_triangle(self):
-        self.assertEqual(a4_exam.ASCII_triangle(3)," *\n***\n")
-        self.assertEqual(a4_exam.ASCII_triangle(5),"  *\n ***\n*****\n")
+        self.assertEqual(a4_midterm.ASCII_triangle(3)," *\n***\n")
+        self.assertEqual(a4_midterm.ASCII_triangle(5),"  *\n ***\n*****\n")
     
     def test_ASCII_square(self):
-        self.assertEqual(a4_exam.ASCII_square(2),"**\n**\n")
-        self.assertEqual(a4_exam.ASCII_square(0),"")
+        self.assertEqual(a4_midterm.ASCII_square(2),"**\n**\n")
+        self.assertEqual(a4_midterm.ASCII_square(0),"")
 
 
     def test_how_much(self):
-        self.assertEqual(a4_exam.how_much(a4_exam.portfolio,a4_exam.Au),52.0)
-        self.assertEqual(a4_exam.how_much(a4_exam.portfolio,a4_exam.Ag),4127.0)
-        self.assertEqual(a4_exam.how_much(a4_exam.portfolio,a4_exam.Pd),42.0)
-        self.assertEqual(a4_exam.how_much(a4_exam.portfolio,a4_exam.Pt),109.0)
+        self.assertEqual(a4_midterm.how_much(a4_midterm.portfolio,a4_midterm.Au),52.0)
+        self.assertEqual(a4_midterm.how_much(a4_midterm.portfolio,a4_midterm.Ag),4127.0)
+        self.assertEqual(a4_midterm.how_much(a4_midterm.portfolio,a4_midterm.Pd),42.0)
+        self.assertEqual(a4_midterm.how_much(a4_midterm.portfolio,a4_midterm.Pt),109.0)
 
 
     def test_purchase(self):
-        a4_exam.purchase(a4_exam.portfolio,a4_exam.Au,2)
-        a4_exam.purchase(a4_exam.portfolio,a4_exam.Ag,20)
-        a4_exam.purchase(a4_exam.portfolio,a4_exam.Pt,5)
-        a4_exam.purchase(a4_exam.portfolio,a4_exam.Pd,10)
-        a4_exam.purchase(a4_exam.portfolio,a4_exam.Au,2)
-        self.assertEqual(a4_exam.portfolio, p)
-        self.assertEqual(a4_exam.worth(a4_exam.portfolio), 100000)
+        a4_midterm.purchase(a4_midterm.portfolio,a4_midterm.Au,2)
+        a4_midterm.purchase(a4_midterm.portfolio,a4_midterm.Ag,20)
+        a4_midterm.purchase(a4_midterm.portfolio,a4_midterm.Pt,5)
+        a4_midterm.purchase(a4_midterm.portfolio,a4_midterm.Pd,10)
+        a4_midterm.purchase(a4_midterm.portfolio,a4_midterm.Au,2)
+        self.assertEqual(a4_midterm.portfolio, p)
+        self.assertEqual(a4_midterm.worth(a4_midterm.portfolio), 100000)
 
     def test_find_num_min(self):
             for i,j in zip(x,y):
-                self.assertEqual(a4_exam.find_num_min(i), j)
+                self.assertEqual(a4_midterm.find_num_min(i), j)
 
 if __name__=="__main__":
     unittest.main()
