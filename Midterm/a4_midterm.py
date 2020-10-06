a2da572b92e69911c9c464a3ae2e703c2a13a6be
gtokusum <gtokusum@iu.edu>
2020-10-06 12:31:09 -0400

    finished everything besides find_num_min

--- Midterm/a4_midterm.py
+++ Midterm/a4_midterm.py
@@ -1,15 +1,36 @@
+import math
 def ASCII_square(n):
-    pass
+    baseSquare = ''
+    k = n
+    while k > 0:
+        baseSquare = baseSquare + ('*' * n) + '\n'
+        k += -1
+    return baseSquare
 
 
 def ASCII_triangle(n):
-    pass
+    basetriangle = ''
+    k = 1
+    pace = n
+    while k <= n:
+        space = (pace/2) - 0.5
+        basetriangle = basetriangle + (' ' * int(space)) + ('*' * k) + '\n'
+        pace -= 2
+        k += 2
+    return basetriangle
 
 
 
 def my_range(start,stop,step):
-    pass
-
+    ap = start
+    sol = [ap]
+    if start < stop:
+        while sol[-1] < stop - step:
+            ap += step
+            sol.append(ap)
+    else:
+        sol = []
+    return sol
 
 Act, Au,Ag,Pd,Pt ="Act", "gold", "silver", "paladium", "platinum"
 spot_price = {Au:1918.40, Ag:24.23,\
@@ -17,13 +38,23 @@ spot_price = {Au:1918.40, Ag:24.23,\
 portfolio = {"Act": 100000, Au:0, Ag:0, Pt:0, Pd:0}
             
 def purchase(portfolio, metal, amt):
-    pass
-
+    metalPrice = spot_price[metal] * amt
+    portfolio["Act"] = portfolio["Act"] - metalPrice
+    portfolio[metal] = portfolio[metal] + amt
+    
 def worth(portfolio):
-    pass
+    liquid = 0
+    portAmt = portfolio["Act"]
+    goldPrice = portfolio[Au] * spot_price[Au]
+    silverPrice = portfolio[Ag] * spot_price[Ag]
+    paladiumPirce = portfolio[Pt] * spot_price[Pt]
+    platinumPrice = portfolio[Pd] * spot_price[Pd]
+    liquid = portAmt + goldPrice + silverPrice + paladiumPirce + platinumPrice
+    return liquid
 
 def how_much(portfolio, metal):
-    pass
+    amtOfMetal = portfolio["Act"] // spot_price[metal]
+    return amtOfMetal
 
 
 def find_num_min(xlst):
